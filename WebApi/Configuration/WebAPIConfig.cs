ac47881422f49b4d366cbcb41214f0f9b1ddb604
rlinsdev <rlinsdev@gmail.com>
2017-11-12 10:48:44 +0100

    Add project files.

---
+++ WebApi/Configuration/WebAPIConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Configuration
+{
+	public static class WebAPIConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			var formatters = GlobalConfiguration.Configuration.Formatters;
+			formatters.Remove(formatters.XmlFormatter);
+		}
+	}
+}
---
+++ WebApi/Controller/HelloController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApi.Controller
+{
+    public class HelloController : ApiController
+    {
+        // GET: api/Hello
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+		// GET: api/Hello/5
+		[HttpGet]
+		[Route("api/Hello/{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		[HttpGet]
+		[Route("api/Hello/{valOne}/{valTwo}")]
+		public string GetMoreParam(int valOne, int valTwo)
+		{
+			return "value with two params";
+		}
+
+		// POST: api/Hello
+		public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Hello/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Hello/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
